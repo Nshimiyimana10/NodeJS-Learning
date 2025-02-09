c4e9e9ee3bfb2f4960d936bacc7cf12306f2b737
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2025-02-09 01:12:18 -0800

    nodemon dependency is installed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { createServer } = require('node:http');
+/*const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -10,4 +10,15 @@ const server = createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
+
+const http = require('http');
+const port = 5500;
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200;
+    res.setHeader('contentType', 'text/plain');
+    res.end('This is just the beginning of my NodeJS journey in backend')
+})
+server.listen(port,()=>{
+    console.log(`The server is running at ${port}`);
+})
